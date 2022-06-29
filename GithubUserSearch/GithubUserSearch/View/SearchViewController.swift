b784cbdf7235863735ef0a75500bb9d671961826
Haeseong <hesung0805@gmail.com>
2022-06-30 00:10:59 +0900

    Git hub User Search Finish

--- GithubUserSearch/GithubUserSearch/View/SearchViewController.swift
+++ GithubUserSearch/GithubUserSearch/View/SearchViewController.swift
@@ -10,9 +10,120 @@ import Combine
 
 class SearchViewController: UIViewController {
     
+    let network = NetworkService(configuration: .default)
+    
+    @Published private(set) var users: [SearchResult] = []
+    var subscription = Set<AnyCancellable>()
+    
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    typealias item = SearchResult
+    var datasource: UICollectionViewDiffableDataSource<Section, item>!
+    enum Section {
+        case main
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        embedSearchControl()
+        configureCollectionView()
+        bind()
+    }
+    
+    private func embedSearchControl() {
+        self.navigationItem.title = "Search"
+        
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.placeholder = "haeseong8880"
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        self.navigationItem.searchController = searchController
+    }
+    
+    private func configureCollectionView() {
+        datasource = UICollectionViewDiffableDataSource<Section, item>(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ResultCell", for: indexPath) as? ResultCell else {return nil }
+            
+            cell.user.text = item.login
+            return cell
+        })
+        
+        collectionView.collectionViewLayout = layout()
+    }
+    
+    private func layout() -> UICollectionViewCompositionalLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(60))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(60))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        
+        return UICollectionViewCompositionalLayout(section: section)
+    }
+    
+    private func bind() {
+        $users
+            .receive(on: RunLoop.main)
+            .sink { users in
+                var snapshot = NSDiffableDataSourceSnapshot<Section, item>()
+                snapshot.appendSections([.main])
+                snapshot.appendItems(users, toSection: .main)
+                self.datasource.apply(snapshot)
+            }.store(in: &subscription)
+
     }
 }
+
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        let keyword = searchController.searchBar.text
+        print("search Text ===> \(keyword)")
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("searchBar Click ===> \(searchBar.text)")
+        
+        guard let keyword = searchBar.text, !keyword.isEmpty else { return }
+//        let base = "https://api.github.com/"
+//        let path = "search/users"
+//        let params: [String: String] = ["q": keyword]
+//        let header: [String: String] = ["Content-Type": "application/json"]
+//        
+//        var urlComponents = URLComponents(string: base + path)!
+//        let queryItem = params.map { (key: String, value: String) in
+//            return URLQueryItem(name: key, value: value)
+//        }
+//        urlComponents.queryItems = queryItem
+//        
+//        var request = URLRequest(url: urlComponents.url!)
+//        header.forEach { (key: String, value: String) in
+//            request.addValue(value, forHTTPHeaderField: key)
+//        }
+        let resource = Resource<SearchUserResponse>(
+            base: "https://api.github.com/",
+            path: "search/users",
+            params: ["q": keyword],
+            header: ["Content-Type": "application/json"])
+        
+        network.load(resource)
+            .map { $0.items }
+            .replaceError(with: [])
+            .receive(on: RunLoop.main)
+            .assign(to: \.users, on: self)
+            .store(in: &subscription)
+//        URLSession.shared.dataTaskPublisher(for: request)
+//            .map { $0.data }
+//            .decode(type: SearchUserResponse.self, decoder: JSONDecoder())
+//            .map { $0.items }
+//            .replaceError(with: [])
+//            .receive(on: RunLoop.main)
+//            .assign(to: \.users, on: self)
+//            .store(in: &subscription)
+    }
+}
+

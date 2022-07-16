09e6e8921576905cbb074cd1e37d3bc85097b0b3
Haeseong <hesung0805@gmail.com>
2022-07-17 01:24:45 +0900

    AppleFramework-SwiftUI Finish

---
+++ AppleFramework-SwiftUI/AppleFramework-SwiftUI/FrameworkListView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  AppleFramework-SwiftUI
+//
+//  Created by joonwon lee on 2022/05/21.
+//
+
+import SwiftUI
+
+struct FrameworkListView: View {
+    
+    @State var list: [AppleFramework] = AppleFramework.list
+    
+    let layout: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+    ]
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: layout) {
+                    ForEach(list) { item in
+                        FrameworkCell(framework: item)
+                    }
+                }
+                .padding([.top, .leading, .trailing], 16.0)
+            }
+            .navigationTitle("⚡️ AppleFramework")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkListView()
+    }
+}
